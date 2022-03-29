41db26ac17f5a9724217931e4d11ffd5292a7eaa
藤原和矢 <kazuya.fujiwara@axas-japan.co.jp>
2022-03-30 00:37:59 +0900

    Chapter6開始 〜 MyQuizボタン作成まで

---
+++ MyQuiz/MyQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by AXLT0220-AP on 2022/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
